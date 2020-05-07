e89c31128797f2a7367b8ed06b171e0887531f71
Sho Mizutani <lowply@github.com>
2020-05-07 14:21:41 +0900

    Update the update script

--- script/update.sh
+++ script/update.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 
 has(){
-	type ${1} > /dev/null 2>&1
-	return $?
+    type ${1} > /dev/null 2>&1
+    return $?
 }
 
 error(){
-	echo "${1}"
-	return 1
+    echo "${1}"
+    exit 1
 }
 
-has "gsed" || error "gsed not installed"
-has "git" || error "git not installed"
-has "gh" || error "gh not installed"
-
+REQUIRED=""
+has "gsed" || REQUIRED="${REQUIRED} gsed"
+has "git" || REQUIRED="${REQUIRED} git"
+has "gh" || REQUIRED="${REQUIRED} gh"
+has "jq" || REQUIRED="${REQUIRED} jq"
+[ -z "${REQUIRED}" ] || error "Some tools are not installed:${REQUIRED}"
 [ -n "${GITHUB_TOKEN}" ] || error "GITHUB_TOKEN is empty"
-
-git tag -l | tail -n 1 | sed -e 's/^v//' | tee VERSION
+[ "$(git branch --show-current)" == 'master' ] || error "Check out the master branch first."
 
 HUGO_VERSION=$(curl -s \
     -H "Accept: application/vnd.github.v3+json" \
     -H "Authorization: token ${GITHUB_TOKEN}" \
     https://api.github.com/repos/gohugoio/hugo/releases/latest \
     | jq -r .tag_name | sed -e 's/^v//')
-CURRENT=$(cat VERSION)
+CURRENT=$(git tag -l | tail -n 1 | sed -e 's/^v//')
 
 echo "       Latest Hugo version: ${HUGO_VERSION}"
 echo "Current Build Hugo version: ${CURRENT}"
 
-[ "${HUGO_VERSION}" == "${CURRENT}" ] && exit 0
+[ "${HUGO_VERSION}" == "${CURRENT}" ] && error "No need to update."
 
 read -p "Proceed to open a pull request to bump to ${HUGO_VERSION}? (y/N): " yn
 case "${yn}" in
