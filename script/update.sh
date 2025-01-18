5145e508c746749e2d1fa6493a2cdd0251be2572
Sho Mizutani <sho@svifa.net>
2025-01-18 11:26:45 +0900

    gh pr create can push to a branch

--- script/update.sh
+++ script/update.sh
@@ -61,5 +61,4 @@ git add .
 git commit -a \
     -m "Update to ${LATEST}" \
     -m "Updating Hugo to [v${LATEST}](https://github.com/gohugoio/hugo/releases/tag/v${LATEST})"
-git push origin "update/${LATEST}"
 gh pr create -f
